41102e728942f1ce97ffba5887d64afc50ea8c58
unknown <jonafi@gmail.com>
2020-04-18 17:56:17 -0500

    begining of countdown functions

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 //start timer
+var quizTimer = 2;
 
+function setTime() {
+  var timerInterval = setInterval(function() {
+    quizTimer--;
+    console.log(quizTimer);
+
+    if(quizTimer === 0) {
+      clearInterval(timerInterval);
+      quizOver();
+    }
+
+  }, 1000);
+}
+
+
+
+
+
+function quizOver() {
+ alert('time up!')
+}
+
+setTime();
 
 //localstorage
 
