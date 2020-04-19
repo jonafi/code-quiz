b4d74e0ce13e1cd9826ed03f3dafd2c7f9982de4
unknown <jonafi@gmail.com>
2020-04-18 22:24:02 -0500

    fleshed out some html and button functionality

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ var timerInterval;
 //DOM manipulation vars
 var displayCountdown = document.querySelector("#timer");
 var startButton = document.querySelector("#startButton");
+var quizContent = document.querySelector("#quizContent");
 
 //begin countdown
 function beginQuiz() {
 timerInterval = setInterval(countDown, 50);
+quiz();
 }
 
 //countdown by one, when 0 trigger quiz over
 function countDown(){
 quizTimer--;
 displayCountdown.innerHTML = quizTimer;
-//console.log(quizTimer);
 if(quizTimer ===0) {
 quizOver();
 }
@@ -24,12 +25,17 @@ quizOver();
 //stop the timer calculate score etc...
 function quizOver() {
   displayCountdown.innerHTML = "TIME UP!"
+  quizContent.innerHTML = "All done<br>your socre is X<br>Enter your intials[  ]";
   clearInterval(timerInterval);
-
 }
 
+//quiz questions
+function quiz(){
 
-addEventListener("click", beginQuiz)
+quizContent.innerHTML = "question 1: why is an orange"; 
+}
+
+startButton.addEventListener("click", beginQuiz)
 
 
 
