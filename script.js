475770b1a0f0aa682fa2e315311eda17a0f13c77
J Ferro <jonafi@gmail.com>
2020-04-20 11:54:02 -0500

    got create element section working

--- script.js
+++ script.js
@@ -10,7 +10,13 @@ var highScores = document.querySelector("#highScores");
 
 //DOM creation vars
 var challengeQuestion = document.createElement("p")
+challengeQuestion.setAttribute("id", "questions")
 var listAnswers = document.createElement("ol");
+listAnswers.setAttribute("id", "choices");
+var listAnswerOption1 = document.createElement("li");
+var listAnswerOption2 = document.createElement("li");
+var listAnswerOption3 = document.createElement("li");
+var listAnswerOption4 = document.createElement("li");
 
 // //Question bank
 var questionBank = {
@@ -47,10 +53,19 @@ function quizOver() {
 //quiz questions
 function quiz(){
 quizContent.innerHTML = "";
-challengeQuestion.textContent="first question goes here";
-listAnswers.innerHTML = "<li>first things</li><li>second thing</li>";
-quizContent.appendChild(challengeQuestion);
-quizContent.appendChild(listAnswers);
+challengeQuestion.innerText ="why is an orange?";
+listAnswerOption1.innerText="because";
+listAnswerOption2.innerText="why not?";
+listAnswerOption3.innerText="ask your mom"
+listAnswerOption4.innerText="herman";
+
+ quizContent.appendChild(challengeQuestion);
+ quizContent.appendChild(listAnswers);
+ listAnswers.appendChild(listAnswerOption1);
+ listAnswers.appendChild(listAnswerOption2);
+ listAnswers.appendChild(listAnswerOption3);
+ listAnswers.appendChild(listAnswerOption4);
+
 }
 
 
